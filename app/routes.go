898a854bc48ca7a6f5ef64053e0fb47b3ae20a09
AdvenAdam <advenadam@student.uns.ac.id>
2024-11-14 11:16:47 +0700

    add router handler using chi

--- app/routes.go
+++ app/routes.go
@@ -3,6 +3,7 @@ package app
 import (
 	"net/http"
 
+	"github.com/AdvenAdam/orders-api/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -14,5 +15,17 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	router.Route("/orders", loadOrderRoutes)
+
 	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
--- main.go
+++ main.go
@@ -1,31 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	// NOTE: This depend on folder name and package name
+	app "github.com/AdvenAdam/orders-api/app"
 )
 
 func main() {
-	router := chi.NewRouter()
+	app := app.New()
 
-	router.Use(middleware.Logger)
-
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Error: ", err)
+		fmt.Println("failed to start application:", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
 }
